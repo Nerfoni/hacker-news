aaa6f1bc1c4a0321758e3cb2e48f883ca0fc9404
Sampo Riikkilä <78560347+Nerfoni@users.noreply.github.com>
2023-09-05 23:56:03 +0300

    Fix imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "@/context/providers";
-import Header from "@/components/header/Header";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
